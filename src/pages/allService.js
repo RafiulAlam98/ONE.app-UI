9bf983fe0a2c7ccaf9d1605cdde25ca05928e534
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2023-10-17 09:40:55 +0600

    Added: All Service Page

--- src/pages/allService.js
+++ src/pages/allService.js
@@ -1,10 +1,118 @@
+/* eslint-disable @next/next/no-img-element */
 import MainLayout from "@/components/Layout/MainLayout";
+import { useGetAllSubCategoryServiceQuery } from "@/redux/slice/subCategoryService/subCategorySlice";
+import { Card, Col, List, Row, Spin, Typography } from "antd";
 import React from "react";
+import styles from "@/styles/AllService.module.css";
+import Link from "next/link";
+import { SafetyOutlined } from "@ant-design/icons";
+
+const { Meta } = Card;
 
 const AllService = () => {
+  const { data, isLoading } = useGetAllSubCategoryServiceQuery();
+  if (isLoading) {
+    return <Spin />;
+  }
+  console.log(data.data);
   return (
-    <div>
-      <h2>this is all services</h2>
+    <div style={{ maxWidth: "1000px", margin: "0 auto" }}>
+      <Row gutter={16}>
+        <Col md={8} lg={8} span={8}>
+          <div
+            style={{
+              fontWeight: 600,
+              textAlign: "center",
+              fontSize: 26,
+              paddingBottom: "16px",
+            }}
+          >
+            All Services
+          </div>
+          <List
+            className={styles.serviceContainer}
+            size="large"
+            dataSource={data.data}
+            renderItem={(item) => (
+              <Link href={`#${item._id}`}>
+                {" "}
+                <List.Item
+                  className={styles.serviceList}
+                  style={{ fontWeight: 600 }}
+                >
+                  {item.title}
+                </List.Item>
+              </Link>
+            )}
+          />
+        </Col>
+        <Col md={16} lg={16} span={16}>
+          {data.data.map((item) => (
+            <div
+              key={item._id}
+              style={{ padding: 16, marginTop: 16, textAlign: "left" }}
+            >
+              <div style={{ margin: 16 }}>
+                <Typography
+                  id={`${item._id}`}
+                  style={{ fontSize: 20, fontWeight: 600, color: "tomato" }}
+                >
+                  {item.title}{" "}
+                </Typography>
+              </div>
+              <div style={{ margin: 16 }}>
+                <Link href={`/serviceDetails/${item._id}`}>
+                  <Card
+                    hoverable
+                    style={{ maxWidth: 240 }}
+                    cover={
+                      <img
+                        alt="example"
+                        style={{ height: 150 }}
+                        src={item.images}
+                      />
+                    }
+                  >
+                    <Meta title={item.category.name} description="" />
+                  </Card>
+                </Link>
+              </div>
+              <div>
+                <List
+                  size="small"
+                  header={
+                    <Typography style={{ fontWeight: 500 }}>
+                      Features
+                    </Typography>
+                  }
+                  dataSource={item.features}
+                  renderItem={(item) => (
+                    <List.Item style={{ fontWeight: 400 }}>
+                      <Typography
+                        style={{
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                        }}
+                      >
+                        {" "}
+                        <SafetyOutlined
+                          style={{
+                            fontSize: 16,
+                            marginRight: 8,
+                            display: "inline-block",
+                          }}
+                        />
+                        <span style={{ display: "inline-block" }}> {item}</span>
+                      </Typography>
+                    </List.Item>
+                  )}
+                />
+              </div>
+            </div>
+          ))}
+        </Col>
+      </Row>
     </div>
   );
 };
--- src/ui/ServiceModalList.jsx
+++ src/ui/ServiceModalList.jsx
@@ -1,4 +1,4 @@
-import { Button, List, Typography } from "antd";
+import { List, Typography } from "antd";
 import Link from "next/link";
 import React from "react";
 
@@ -6,33 +6,31 @@ const ServiceModalList = ({ value }) => {
   console.log(value._id);
   return (
     <>
-      <Link href={``}>
-        <List
-          size="small"
-          style={{
-            textAlign: "right",
-            boxShadow:
-              "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
-          }}
-          bordered
-          dataSource={value}
-          renderItem={(item) => (
-            <Link href={`/serviceDetails/${item._id}`}>
-              <List.Item>
-                <Typography
-                  style={{
-                    fontWeight: 400,
-                    color: "black",
-                    fontSize: "16px",
-                  }}
-                >
-                  {item.title}
-                </Typography>
-              </List.Item>
-            </Link>
-          )}
-        />
-      </Link>
+      <List
+        size="small"
+        style={{
+          textAlign: "right",
+          boxShadow:
+            "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
+        }}
+        bordered
+        dataSource={value}
+        renderItem={(item) => (
+          <Link href={`/serviceDetails/${item._id}`}>
+            <List.Item>
+              <Typography
+                style={{
+                  fontWeight: 400,
+                  color: "black",
+                  fontSize: "16px",
+                }}
+              >
+                {item.title}
+              </Typography>
+            </List.Item>
+          </Link>
+        )}
+      />
     </>
   );
 };
